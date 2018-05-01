11c6a59a58f310d9e954c2d107c10fe655b1f464
carriecarter <carrie@carrie-carter.com>
2018-05-01 14:50:11 -0700

    trying out my first function on the page

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 'use strict'
 
+function designServices(){
+    var logo = prompt('Do you need a logo?');
+    console.log('Do they need a logo designed?', logo);
+
+    var p = document.getElementById('logo-response');
+
+    if(logo.trim().toLowerCase() === 'yes') {
+        p.textContent = 'Great! I can do that for you.';
+    }
+    else {
+        p.textContent = 'No problem, let\'s see what else you might need.';
+    }
+}
+
