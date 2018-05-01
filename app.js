8baa3e479d41073fdfa2eb2f38965ac039010c05
carriecarter <carrie@carrie-carter.com>
2018-05-01 15:57:19 -0700

    scaling back my content to keep it simpler for my brain to grok

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict'
 
-function designServices(){
-    var logo = prompt('Do you need a logo?');
-    console.log('Do they need a logo designed?', logo);
+function projects(){
+    // Yes No Question 1
+    var logo = prompt('Did I design the Next Adventure logo?');
+    console.log('Did I design the Next Adventure logo?', logo);
 
     var p = document.getElementById('logo-response');
 
-    if(logo.trim().toLowerCase() === 'yes') {
-        p.textContent = 'Great! I can do that for you.';
+    if(logo.trim().toLowerCase() === 'no' === 'No' === 'N') {
+        p.textContent = 'I did not design the PCT Days logo, but I wish I could have had a shot at it.';
     }
     else {
         p.textContent = 'No problem, let\'s see what else you might need.';
     }
+    //Yes No Question 2
+    var letterpress = prompt('Do you need letterpress wedding invitations?');
+    console.log('are they looking for letterpress services?', letterpress);
+
+    var p = document.getElementById('letterpress-response');
+
+    if(letterpress.)
+
 }
 
